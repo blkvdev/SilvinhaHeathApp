524d002a474ca973f1c19b1b1788a9adcaaedf7b
blkdev <carlosfilhocontato2008@gmail.com>
2025-02-27 12:27:24 -0300

    Estilizando a tela inicial

--- SilvinhaHealthApp/App.js
+++ SilvinhaHealthApp/App.js
@@ -1,21 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <View style={styles.titleBox}>
-      <Text style={styles.titleText}>Silvinha Health App</Text>
+        <Text style={styles.titleText}>Silvinha Health App</Text>
       </View>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    paddingTop:80
+    backgroundColor: '#ef2333c',
+  },
+  titleBox: {
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    height: 100,
+    backgroundColor: '#000',
+    borderBottomStartRadius: 50,
+    borderBottomEndRadius: 50,
+  },
+  titleText:
+  {
+    color: '#aff',
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginBottom: 20,
   },
 });
